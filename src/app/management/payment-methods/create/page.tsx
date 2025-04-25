5d48c33571a4897052ffc60c320a7a3e26e3b1b6
pjimenez2510 <pjimenez2510@gmail.com>
2025-04-24 20:39:41 -0500

    feat: Add create and edit views for payment methods, along with form hooks and constants

---
+++ src/app/management/payment-methods/create/page.tsx
@@ -0,0 +1,5 @@
+import PaymentMethodCreateView from "@/features/payment-methods/presentation/views/payment-method-create-view";
+
+export default function Page() {
+  return <PaymentMethodCreateView />;
+}
